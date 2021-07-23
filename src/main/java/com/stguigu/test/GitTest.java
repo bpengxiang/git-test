e52ab6ed17cfbcd7e23cb6079f71c532f6f67180
xiangzi <xiangzi@qq.com>
2021-07-23 20:30:38 +0800

    er commit

--- src/main/java/com/stguigu/test/GitTest.java
+++ src/main/java/com/stguigu/test/GitTest.java
@@ -2,6 +2,6 @@ package com.stguigu.test;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git");
+        System.out.println("hello git");
     }
 }
