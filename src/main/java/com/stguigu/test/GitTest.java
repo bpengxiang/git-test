6a8554ed6a4360180548ee88d4bb8bbf0f742064
xiangzi <xiangzi@qq.com>
2021-07-23 20:40:59 +0800

    hotfix firstcommit

--- src/main/java/com/stguigu/test/GitTest.java
+++ src/main/java/com/stguigu/test/GitTest.java
@@ -3,5 +3,6 @@ package com.stguigu.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hot fix");
     }
 }
